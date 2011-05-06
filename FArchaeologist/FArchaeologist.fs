a3cc45f7f4a9446128ab1c2ecc15f31ee85ae4c7
Ryan Riley <ryan.riley@panesofglass.org>
2011-05-06 16:29:39 -0700

    Added runner, libraries, and basic Frack application.

---
+++ FArchaeologist/FArchaeologist.fs
@@ -0,0 +1,29 @@
+﻿module FArchaeologist
+
+[<AutoOpen>]
+module Dynamic =
+  let inline (?) this key =
+    ( ^a : (member get_Item : ^b -> ^c) (this,key))
+  
+  let inline (?<-) this key value =
+    ( ^a : (member set_Item : ^b * ^c -> ^d) (this,key,value))
+
+type MongoConfig = {
+  Host : string
+  Port : int
+  Username : string
+  Password : string }
+  with member x.ConnectionString = sprintf "mongodb://%s:%s@%s:%d" x.Username x.Password x.Host x.Port
+
+module AltNetMiner =
+  open MongoDB
+
+  let connect (config: MongoConfig) =
+    let mongo = new Mongo(config.ConnectionString)
+    mongo.Connect()
+    mongo
+
+  let getDiscussions config =
+    let mongo = connect config
+    mongo?AltNetMiner?AltNetSeattleDiscussions
+
---
+++ Runner/Program.fs
@@ -0,0 +1,15 @@
+﻿module Program
+
+open System.Linq
+open FArchaeologist
+open MongoDB
+
+let config = {
+  Host = "flame.mongohq.com"
+  Port = 27018
+  Username = "darkxanthos"
+  Password = "abc123!" }
+
+AltNetMiner.getDiscussions(config).Linq().AsEnumerable()
+  |> Seq.countBy (fun d -> d?sender)
+  |> Seq.iter (printfn "%A")
