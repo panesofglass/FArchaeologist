a29043acd39792960a6b7b9b90012a79b7707533
Ryan Riley <ryan.riley@panesofglass.org>
2011-05-06 17:03:32 -0700

    Replicated conversation_edges; might need some date tweaking.

--- FArchaeologistWeb/Core/Global.fs
+++ FArchaeologistWeb/Core/Global.fs
@@ -2,10 +2,14 @@
 
 open System
 open System.Collections.Generic
+open System.Linq
 open System.Web
 open System.Web.Routing
+open FArchaeologist
 open Frack
 open Frack.Hosting.AspNet
+open MongoDB
+open Newtonsoft.Json
 
 type Global() =
   inherit System.Web.HttpApplication() 
@@ -14,9 +18,15 @@ type Global() =
     // Echo the request body contents back to the sender. 
     // Use Fiddler to post a message and see it return.
     let app (request: IDictionary<string, obj>) = async {
-      let greeting = "Howdy!\r\n"
-      return "200 OK", dict [("Content-Type", "text/html")],
-             Sequence(seq { yield (Str greeting) }) }
+      let config = {
+        Host = "flame.mongohq.com"
+        Port = 27018
+        Username = "darkxanthos"
+        Password = "abc123!" }
+      
+      let message = AltNetMiner.getDiscussions(config).Linq().ToArray() |> JsonConvert.SerializeObject
+
+      return "200 OK", dict [("Content-Type", "text/html")], Str message }
 
     // Uses the head middleware.
     // Try using Fiddler and perform a HEAD request.
